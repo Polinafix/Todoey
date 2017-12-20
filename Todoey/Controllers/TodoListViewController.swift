df0827094db7248c6a8088c7c46067ef68563c70
polinafiksson <pd.fiksson@gmail.com>
2017-12-20 13:58:49 -0600

    Persisting data using CoreData.

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -16,13 +16,21 @@ class TodoListViewController: UITableViewController {
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     //mutable array
     var itemArray = [Item]()
+    //will be nil until we set it in another VC
+    //but once we set the selected category that's when we want to load up all the items relrvant to this specific category > use the computed property
+    var selectedCategory: Category? {
+        //will happen as soon as selected category gets set with the value
+        didSet{
+            loadItems()
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
+        //print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
         //here we want to load up all of the to-do items from our persistent container
-        loadItems()
+        //loadItems()
             }
     
     //MARK: - tableView DataSource methods
@@ -71,6 +79,8 @@ class TodoListViewController: UITableViewController {
                 let newItem = Item(context: self.context)
                 newItem.title = myItem
                 newItem.done = false
+                //specify the category
+                newItem.parentCategory = self.selectedCategory
                 self.itemArray.append(newItem)
                 self.saveItems()
             }
@@ -90,9 +100,7 @@ class TodoListViewController: UITableViewController {
     //MARK: - Model manipulation methods
     
     func saveItems() {
-        //commit our temp context to the permanent storage inside the persistent container
-        //context.save()
-        
+        //commit our temp context to the permanent storage
         do {
             try context.save()
         }catch{
@@ -102,13 +110,22 @@ class TodoListViewController: UITableViewController {
     }
     
     //if we call this function and don't provide a value for the request, then we can have a default value
-    func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest()) {
+    func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest(), predicate: NSPredicate? = nil) {
+        //only load the items that have a parent category matching the selected one > query
+        let categoryPredicate = NSPredicate(format: "parentCategory.name MATCHES %@", selectedCategory!.name!)
+        //get all the predicates together
+        if let additionalPredicate = predicate {
+            request.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [categoryPredicate,additionalPredicate])
+        }else {
+            request.predicate = categoryPredicate
+        }
         //first, our app needs to talk to the context
         do {
             try itemArray = context.fetch(request)//output for this method id going to be an array of items that is stored in the persistent container
         }catch {
             print("Error fetching data from the context \(error)")
         }
+        
         
         tableView.reloadData()
         
@@ -120,24 +137,28 @@ class TodoListViewController: UITableViewController {
 extension TodoListViewController: UISearchBarDelegate {
     
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        //In order to read from the context we always need to create a request and we have to declare its data type:
-        
+        //In order to read from the context we always need to create a request
         let request:NSFetchRequest<Item> = Item.fetchRequest()
-    
-        //we need to specify what our filter is(query)
-        //for all the items in Items array look for the once where the title contains the enter text
-         //add query to our request
-        request.predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+        //for all the items in Items array look for the once where the title contains the enter text, add query to our request
+        let predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
         /*NSPredicate is a foundation class that specifies how data should be fetched or filtered*/
  
-        //sort the data that we get from the DB
-        //add it to the request:
+        //sort the data that we get from the DB, add it to the request:
         request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
-        
-        //run our request and fetch the results
+
         //will return only those that satisfy the rules that we specified for our request
-        loadItems(with: request)
-        
+        loadItems(with: request, predicate: predicate)
+    }
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        //once we delete the text, load up all the items
+        if searchBar.text!.count == 0 {
+            loadItems()
+            
+            DispatchQueue.main.async {
+                 searchBar.resignFirstResponder()
+            }
+           
+        }
     }
 }
 
